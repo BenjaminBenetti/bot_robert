83b1a2def2fc4fc3918b74e15f1268017d64fb6c
CanadianCommander <ben.benetti@shaw.ca>
2021-07-19 22:34:42 -0700

    add source command to get GH link

--- bot_robert_lib/src/slash_command/command_ingest.rs
+++ bot_robert_lib/src/slash_command/command_ingest.rs
@@ -8,15 +8,20 @@ use crate::slash_command::joke_handler;
 /// ### return
 /// a string response to send to the user
 pub fn process_command(user_name: &String, args: &String) -> String {
+    // totally maintainable :P
     let robert_regex = Regex::new(r"^[rR]obert.*");
     let joke_regex = Regex::new(r"^\s*joke.*");
+    let source_regex = Regex::new(r"source");
 
-    if let (Ok(joke_reg), Ok(robert_reg)) = (joke_regex, robert_regex) {
+    if let (Ok(joke_reg), Ok(robert_reg), Ok(source_reg)) = (joke_regex, robert_regex, source_regex) {
         if robert_reg.is_match(&user_name) {
             String::from("I'm talking to my self!")
         }
         else if joke_reg.is_match(&args) {
             joke_handler::tell_joke(&args)
+        }
+        else if source_reg.is_match(&args) {
+            String::from("https://github.com/CanadianCommander/bot_robert")
         }
         else {
             String::from("I dont understand! This is an HR violation!")
