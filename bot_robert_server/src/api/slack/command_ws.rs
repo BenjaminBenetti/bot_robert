74513b768dcf0d27b91954af828678d445fa41dd
CanadianCommander <ben.benetti@shaw.ca>
2021-07-20 22:01:05 -0700

    change response to JSON. Make response visible to channel!

--- bot_robert_server/src/api/slack/command_ws.rs
+++ bot_robert_server/src/api/slack/command_ws.rs
@@ -1,8 +1,11 @@
 use actix_web::{post, web, HttpResponse, Responder};
-use crate::api::slack::transfer::SlashCommandTransfer;
+use crate::api::slack::transfer::{SlashCommandTransfer, SlashCommandResponseTransfer, SlashCommandResponseType};
 
 /// handles incoming slash command from slack.
 #[post("/api/slack/cmd")]
 pub async fn slash_command(slash_cmd: web::Form<SlashCommandTransfer>) -> impl Responder {
-    HttpResponse::Ok().body(bot_robert_lib::slash_command::process_command(&slash_cmd.user_name, &slash_cmd.text))
+    let response = bot_robert_lib::slash_command::process_command(&slash_cmd.user_name, &slash_cmd.text);
+
+    let transfer = SlashCommandResponseTransfer{text: response, response_type: String::from(SlashCommandResponseType::InChannel.to_s())};
+    HttpResponse::Ok().json(transfer)
 }
--- bot_robert_server/src/api/slack/transfer.rs
+++ bot_robert_server/src/api/slack/transfer.rs
@@ -1,2 +1,5 @@
 mod slash_command_transfer;
+mod slash_command_response_transfer;
+
 pub use slash_command_transfer::*;
+pub use slash_command_response_transfer::*;
